5a7b8651541685119ed64eb97e878dc2c8d58c7b
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-12-10 19:19:17 +0900

    Add:ログインユーザのIDを取得する

--- src/components/fire.js
+++ src/components/fire.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
@@ -12,3 +13,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
