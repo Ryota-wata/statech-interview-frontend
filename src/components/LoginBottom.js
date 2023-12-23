43997a9c5cb3de66a3da79147c131811958939ea
mishina naoya <msnyxx811@gmail.com>
2023-12-23 11:01:46 +0900

    ログイン状態をアプリ全体で維持

--- src/components/LoginBottom.js
+++ src/components/LoginBottom.js
@@ -1,127 +1,23 @@
-import { useEffect, useState } from 'react';
-import { initializeApp } from '@firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from '@firebase/auth';
-
-import Button from '../components/Button';
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_APIKEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-  appId: process.env.NEXT_PUBLIC_APPID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
-};
-
-const auth = getAuth(initializeApp(firebaseConfig));
-const provider = new GoogleAuthProvider();
+import React from 'react';
+import Button from './Button';
+import { useAuth } from './Auth';
 
 const LoginBottom = () => {
-  const [state, setState] = useState('idle');
-  const [error, setError] = useState('');
-  const [credential, setCredential] = useState(null);
-
-  const handleLogin = async () => {
-    try {
-      setState('progress');
-      const result = await signInWithPopup(auth, provider);
-      setCredential(result);
-      setState('logined');
-    } catch (e) {
-      setError(e);
-      setState('error');
-    }
-  };
-
-  const handleLogout = async () => {
-    try {
-      setState('idle');
-      await signOut(auth);
-      setState('logouted');
-      setCredential(null);
-    } catch (e) {
-      setError(e);
-      setState('error');
-    }
-  };
-
-  const sendTokenToServer = (token) => {
-    // ユーザートークンをHTTPリクエストを使用してサーバーに送信するロジックを実装します（例：fetchを使用）。
-    // プレースホルダーのURLは、実際のサーバーエンドポイントに置き換えてください。
-    const serverEndpoint = `${process.env.NEXT_PUBLIC_APP_SERVER_URL}/login`;
-    fetch(serverEndpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
-      }
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('ユーザートークンのサーバーへの送信に失敗しました');
-        }
-      })
-      .catch(error => {
-        console.error('サーバーへのトークンの送信中にエラーが発生しました:', error);
-      });
-  };
-
-  useEffect(() => {
-    const token = sessionStorage.getItem('token');
-    if (!credential && token && state!='logouted') {
-      handleLogin();
-    }
-  }, [credential]);
-
-  useEffect(() => {
-    const handleTokenUpdate = async () => {
-      if (credential) {
-        try {
-          const idToken = await credential.user.getIdToken();
-          sessionStorage.setItem('token', idToken);
-          sendTokenToServer(idToken);
-        } catch (error) {
-          console.error('IDトークンの取得中にエラーが発生しました:', error);
-        }
-      } else {
-        sessionStorage.removeItem('token');
-      }
-    };
-  
-    handleTokenUpdate();
-  }, [credential]);
+  const { user, login, logout } = useAuth();
 
   return (
     <div>
-      {state === 'logined' ? (
+      {user ? (
         <div>
-          <Button
-          color="warning"
-          variant="contained"
-          size="small"
-          component="a"
-          href="/me"
-          sx={{ marginRight: 2 }}
-        >
-          戦歴
-        </Button>
-        <Button
-          color="error"
-          variant="contained"
-          size="small"
-          onClick={handleLogout}
-        >
-          ログアウト
-        </Button>
-      </div>
+          <Button color="warning" variant="contained" size="small" component="a" href="/me" sx={{ marginRight: 2 }}>
+            戦歴
+          </Button>
+          <Button color="error" variant="contained" size="small" onClick={logout}>
+            ログアウト
+          </Button>
+        </div>
       ) : (
-        <Button
-          color="error"
-          variant="contained"
-          size="small"
-          onClick={handleLogin}
-        >
+        <Button color="error" variant="contained" size="small" onClick={login}>
           ログイン
         </Button>
       )}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-// import '@/styles/globals.css'
+import AppAppBar from './AppAppBar';
+import { AuthProvider } from '../components/Auth';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <AuthProvider>
+      <AppAppBar />
+      <Component {...pageProps} />
+    </AuthProvider>
+  </>
+  )
 }
